e9defe2957c8e7a07c12578d5df4c2f919d966e9
10xOXR <10xoxr@gmail.com>
2019-04-02 21:26:05 +0100

    Readded Debug Mode

--- app.py
+++ app.py
@@ -9,4 +9,5 @@ def hello():
 
 if __name__ == "__main__":
         app.run(host=os.environ.get("IP"),
-        port=os.environ.get("PORT"))
+        port=os.environ.get("PORT"),
+        debug=True)
